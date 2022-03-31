1c47ad2274264030aae61675785de3002eb16753
Eric Paoli <ericpaoli65@gmail.com>
2022-03-31 22:43:42 +0200

    login and signup form ok, add bcrypt and uid2 for security little styling error badge

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,14 @@
 var express = require('express');
+////// MODELS ///////
 const pinsModel = require('../models/pins');
-var router = express.Router();
-var uniqid = require ('uniqid');
+const userModel = require ('../models/users')
 
+var router = express.Router();
+/////// SECURITY ///////
+// const uniqid = require ('uniqid');
+const bcrypt = require("bcrypt");
+const uid2 = require("uid2");
+/////// CLOUDINARY ///////
 var cloudinary = require('cloudinary').v2;
 cloudinary.config({ 
   cloud_name: 'ericcloud', 
@@ -10,21 +16,21 @@ cloudinary.config({
   api_secret: '2RLSoyrSOUzcxhLF-mCvNHWc2_s' 
 });
 
-router.post("/upload", async function (req, res) {
-  let pictureName = './tmp' + uniqid() + '.jpg';
-  let resultCopy = await req.files.avatar.mv(pictureName);
+// router.post("/upload", async function (req, res) {
+//   let pictureName = './tmp' + uniqid() + '.jpg';
+//   let resultCopy = await req.files.avatar.mv(pictureName);
 
-  if (!resultCopy) {
-    var resultCloudinary = await cloudinary.uploader.upload(pictureName);
-    var options = {
-      json: {
-        apiKey: '2RLSoyrSOUzcxhLF-mCvNHWc2_s',
-        image: resultCloudinary.url,
-      },
+//   if (!resultCopy) {
+//     var resultCloudinary = await cloudinary.uploader.upload(pictureName);
+//     var options = {
+//       json: {
+//         apiKey: '2RLSoyrSOUzcxhLF-mCvNHWc2_s',
+//         image: resultCloudinary.url,
+//       },
 
-    }
-  }
-})
+//     }
+//   }
+// })
 
 
 /* GET home page. */
@@ -32,7 +38,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 
 });
-
+/////////// pin creation route ///////
 router.post('/pins', async function (req, res) {
   let newpin;
 
@@ -46,10 +52,73 @@ router.post('/pins', async function (req, res) {
 
   res.json({newpin})
 })
-
+/////// get pins on app opening ///////
 router.get('/recuppin', async function (req, res) {
   let recuppin = await pinsModel.find()
   res.json({recuppin})
 })
+/////// signup ///////
+router.post("/signup", async function (req, res) {
+  let newUser= null;
+  let result = false
+  let error = []
+  let hash = bcrypt.hashSync(req.body.password, 10);
+  /////// check if the user is already register in DB ///////
+  const userdata = await userModel.findOne({
+    email: req.body.email,
+    firstName: req.body.firstname,
+    lastName: req.body.lastname,
+  })
+  /////// if he's already record the error is diplayed ///////
+  if (userdata != null){
+    error.push('cet utilisateur est déja présent')
+  }
+  /////// check server side validation ///////
+  if( req.body.email === '' || req.body.firstname === '' || req.body.lastname === '' ) {
+    error.push('veuillez verifier vos informations')
+  }
+  /////// if everything is alright, the user is send to DB ///////
+  if (error.length === 0) {
+    let userSignup = new userModel({
+    email: req.body.email,
+    firstName: req.body.firstname,
+    lastName: req.body.lastname,
+    password: hash,
+    token: uid2(32)
+    })
+    newUser = await userSignup.save() 
+    if(newUser) {
+    result = true
+    }
+  }
+  res.json({newUser, result, error})
+})
+
+
+/////// login ///////
+router.post('/login', async function (req, res) {
+  let result = false;
+  let user = null;
+  let error = [];
+  let token = '';
+/////// check server side validations /////// 
+  if(req.body.email === '' && req.body.password === '') {
+    error.push('champs vides')
+    console.log("error///", error)
+  }
+/////// is everything is ok the server send request to DB ///////
+  if(error.length === 0) {
+    user = await userModel.findOne({email : req.body.email,})
+    if(bcrypt.compareSync(req.body.password, user.password)) {
+      result=true
+      token = user.token
+    } else {
+      error.push('veuillez verifier vos identifiants')
+      console.log("error connex", error)
+    }
+  }
+  res.json({result, user, error, token})
+})
+
 
 module.exports = router;
